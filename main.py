f678c96b8b8806b57968d57a6dc25d2826858305
eversonpg <74927464+eversonpg@users.noreply.github.com>
2020-12-18 15:41:51 +0000

    Atividade 1- Grupo 2

--- main.py
+++ main.py
@@ -4,6 +4,16 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    número_1 = float(input("Digite um número\n"))
+    número_2 = float(input("Digite outro número\n"))
+
+    #maior
+    if (número_1 >= número_2):
+       maior = número_1
+    else:
+      maior = número_2
+    print(f'{maior}')
+
 
 if __name__ == '__main__':
     main()
